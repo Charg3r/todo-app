be60fde32a83d1fb6b1e298949e5910d0e2a939d
Carlos <carlosgolivas@gmail.com>
2021-12-19 13:24:12 -0600

    Migraciones

---
+++ database/migrations/2021_12_17_232459_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('tag')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->dateTime('due_date')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
