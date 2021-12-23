1db6f9776b89abd3c755ee0117082f59d7b01c78
Carlos <carlosgolivas@gmail.com>
2021-12-23 17:19:09 -0600

    User API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,17 +20,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// GET -> Regresa información breve de todas las tareas
+// GET -> Returns all tasks
 Route::get('/tasks', [TaskController::class, 'index']);
 
 Route::prefix('/task')->group (function () {
-    // GET -> Regresa toda la información de una tarea
+    // GET -> Returns a task with a given id.
     Route::get('/{id}', [TaskController::class, 'show']);
-    // POST -> Crear una tarea
+    // POST -> Creates a new task
     Route::post('/store', [TaskController::class, 'store']);
-    // PUT -> Editar una tarea
+    // PUT -> Update a task with a given id
     Route::put('/{id}', [TaskController::class, 'update']);
-    // DELETE -> Borrar una tarea
+    // PUT -> Toggle complete a task with a given id
+    Route::put('/status/{id}', [TaskController::class, 'toggleComplete']);
+    // DELETE -> Delete a task with a given id
     Route::delete('/{id}', [TaskController::class, 'destroy']);
     }
 );
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
